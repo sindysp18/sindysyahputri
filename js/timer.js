df5296cca7211baa4d2176b2665021b3e699503b
sindysp18 <64756487+sindysp18@users.noreply.github.com>
2020-05-04 07:16:58 +0700

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -10,7 +10,7 @@ window.onload = function() {
 };
 
 function update_time() {
-	/* Start Time: 2002.05.05 00:00 */
+	/* Start Time: 2020.05.05 00:00 */
 	var start_itme = 20200505;
 	var duration = parseInt((Date.now() / - start_itme ), 10);
 
